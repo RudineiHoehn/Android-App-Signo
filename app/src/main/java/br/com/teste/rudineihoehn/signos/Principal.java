cdf5f19545771275099150af46a7b304df9caf4c
Rudinei Hoehn <rudineilh@gmail.com>
2017-08-23 13:12:35 -0300

    Inicio projeto.

---
+++ app/src/main/java/br/com/teste/rudineihoehn/signos/Principal.java
@@ -0,0 +1,30 @@
+package br.com.teste.rudineihoehn.signos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class Principal extends AppCompatActivity {
+
+    private Spinner spinnerDia = null;
+    private Spinner spinnerMes = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+
+        spinnerDia = (Spinner) findViewById(R.id.spinnerDia);
+        spinnerMes = (Spinner) findViewById(R.id.spinnerMes);
+
+        ArrayAdapter<CharSequence> adapter_dia = ArrayAdapter.createFromResource(this, R.array.spinner_dias, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adapter_mes = ArrayAdapter.createFromResource(this, R.array.spinner_mese, android.R.layout.simple_spinner_item);
+
+        adapter_dia.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter_mes.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinnerDia.setAdapter(adapter_dia);
+        spinnerMes.setAdapter(adapter_mes);
+    }
+}
